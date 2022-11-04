ab4bd086201de0b6cc14dde652751036635d16ab
benny <kluge0221@gmail.com>
2022-11-04 22:32:37 +0900

    ViewModel 추가

--- app/src/main/java/com/example/composetestbox/MainActivity.kt
+++ app/src/main/java/com/example/composetestbox/MainActivity.kt
@@ -11,14 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetestbox.ui.theme.ComposeTestBoxTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestBoxTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
